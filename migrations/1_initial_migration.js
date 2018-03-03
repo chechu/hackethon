36b5fb786ffd536cbcbaf0e43128447481cccfe4
Hernando Bocanegra <hbocanegra@kenovix.com>
2018-03-03 18:19:44 +0100

    Se agrega interacción UI - contract.

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,13 @@
-var Migrations = artifacts.require("./Migrations.sol");
+var Migrations = artifacts.require("Migrations");
 
 module.exports = function(deployer) {
+  // Deploy the Migrations contract as our only task
   deployer.deploy(Migrations);
+};
+
+var Grade = artifacts.require("Grade");
+
+module.exports = function(deployer) {
+  // Deploy the Migrations contract as our only task
+  deployer.deploy(Grade);
 };
--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ App = {
 
   init: function() {
     // Load pets.
-    $.getJSON('../pets.json', function(data) {
+    /*$.getJSON('../pets.json', function(data) {
       var petsRow = $('#petsRow');
       var petTemplate = $('#petTemplate');
 
@@ -18,23 +18,36 @@ App = {
 
         petsRow.append(petTemplate.html());
       }
-    });
+    });*/
 
     return App.initWeb3();
   },
 
   initWeb3: function() {
-    /*
-     * Replace me...
-     */
+    // Is there an injected web3 instance?
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+    } else {
+      // If no injected web3 instance is detected, fall back to Ganache
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+    }
+    web3 = new Web3(App.web3Provider);
 
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    $.getJSON('Grade.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with truffle-contract
+      var GradeArtifact = data;
+      App.contracts.Grade = TruffleContract(GradeArtifact);
+    
+      // Set the provider for our contract
+      App.contracts.Grade.setProvider(App.web3Provider);
+    
+      // Use our contract to retrieve and mark the adopted pets
+      return App.markGrade();
+    });
 
     return App.bindEvents();
   },
@@ -43,10 +56,22 @@ App = {
     $(document).on('click', '.btn-adopt', App.handleAdopt);
   },
 
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
+  markGrade: function(marks, account) {
+    var gradeInstance;
+
+    App.contracts.Grade.deployed().then(function(instance) {
+      gradeInstance = instance;
+    
+      return gradeInstance.getMark.call();
+    }).then(function(marks) {
+      for (i = 0; i < marks.length; i++) {
+        if (marks[i] !== '0x0000000000000000000000000000000000000000') {
+          $('.panel-pet').eq(i).find('button').text('Success').attr('disabled', true);
+        }
+      }
+    }).catch(function(err) {
+      console.log(err.message);
+    });
   },
 
   handleAdopt: function(event) {
